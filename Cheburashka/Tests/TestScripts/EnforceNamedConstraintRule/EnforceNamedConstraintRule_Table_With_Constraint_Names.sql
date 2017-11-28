5cd4c7c03ae2962d9e413f56a1eb8998b42c3fcf
Mark Buckle <mark.buckle@cap-hpi.com>
2017-11-28 10:00:14 +0000

    Added constraint names test cases

---
+++ Cheburashka/Tests/TestScripts/EnforceNamedConstraintRule/EnforceNamedConstraintRule_Table_With_Constraint_Names.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE dbo.EnforceNamedConstraintRule_Table_With_Constraint_Names 
+(       a int constraint pk_EnforceNamedConstraintRule_Table_With_Constraint_Names primary key 
+,       b int constraint ck_EnforceNamedConstraintRule_Table_With_Constraint_Names check ( b > 0 )
+,       c int constraint df_EnforceNamedConstraintRule_Table_With_Constraint_Names default (0)
+,       d int constraint fk_EnforceNamedConstraintRule_Table_With_Constraint_Names foreign key references dbo.EnforceNamedConstraintRule_Table_Without_Constraint_Names (a)
+) ;
+GO
