e9add028fdb95fc63a78868aee4c0692b6f238f4
Ded Medved <dedmedved@hotmail.co.uk>
2021-02-09 17:08:04 +0000

    ditto

--- Cheburashka/Tests/TestScripts/DisallowAllCodeManipulationOfProjectDefinedObjects_MixedCaseRule/ProcedureWithDropIndex.sql
+++ Cheburashka/Tests/TestScripts/DisallowAllCodeManipulationOfProjectDefinedObjects_MixedCaseRule/ProcedureWithDropIndex.sql
@@ -5,9 +5,12 @@ create table  dbo.[Table1]
 go
 create index ix_Table_01 on dbo.[Table1] (b)
 go
+create index ix_Table_02 on dbo.[Table1] (b)
+go
 CREATE proc [ProcedureWithDropIndex]
     AS
     BEGIN
         drop index IX_TABLE_01 on DBO.TABLE1
+        drop index IX_TABLE_02 on TABLE1
     END
 go
