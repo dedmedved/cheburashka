202e8c21f995dd28f1e3d19374dc0eae5f8ca7e9
Ded MedVed <dm@dm.org>
2021-02-27 19:35:44 +0000

    Updated references. Added memopt fktest

---
+++ Cheburashka/Tests/TestScripts/EnforceForeignKeyNonStandardTableRule/EnforceForeignKeyRule_MemoryOptimisedTable_With_FK.sql
@@ -0,0 +1,14 @@
+﻿create TABLE dbo.EnforceForeignKeyRule_MemoryOptimisedTable_With_FK (a int not NULL
+, CONSTRAINT PK_EnforceForeignKeyRule_MemoryOptimisedTable_With_FK primary KEY (a)
+)
+WITH (MEMORY_OPTIMIZED = ON, DURABILITY = SCHEMA_AND_DATA)
+GO
+create table dbo.EnforceForeignKeyRule_MemoryOptimisedTable_With_FK_Child (a int not NULL
+, CONSTRAINT PK_EnforceForeignKeyRule_MemoryOptimisedTable_With_FK_Child primary KEY (a)
+, constraint FK_EnforceForeignKeyRule_MemoryOptimisedTable_With_FK foreign key (a) references EnforceForeignKeyRule_MemoryOptimisedTable_With_FK (a)
+)
+WITH (MEMORY_OPTIMIZED = ON, DURABILITY = SCHEMA_AND_DATA)
+go
+
+-- as of today 23/02/2021 SSDT doesn't support temp/generic system_versioing history tables - ie 
+-- a name needs to be given for HISTORY_TABLE
