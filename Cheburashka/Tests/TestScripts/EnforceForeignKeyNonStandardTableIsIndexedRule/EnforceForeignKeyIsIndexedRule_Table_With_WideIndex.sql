6a70d40d6c66a777dd8a75231140a8523745eeec
DM <DM@DM>
2021-03-11 17:10:27 +0000

    Created skeleton test cases from main test cases.

---
+++ Cheburashka/Tests/TestScripts/EnforceForeignKeyNonStandardTableIsIndexedRule/EnforceForeignKeyIsIndexedRule_Table_With_WideIndex.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE dbo.EnforceForeignKeyIsIndexedRule_Table_With_WideIndex (a INT
+, CONSTRAINT PK_EnforceForeignKeyIsIndexedRule_Table_With_WideIndex primary KEY (a)
+) ;
+GO
+create table EnforceForeignKeyIsIndexedRule_Table_With_WideIndex_Child (a INT
+, b INT 
+, constraint FK_EnforceForeignKeyIsIndexedRule_Table_With_WideIndex foreign key (a) references EnforceForeignKeyIsIndexedRule_Table_With_WideIndex (a)
+)
+GO
+CREATE INDEX ix_EnforceForeignKeyIsIndexedRule_Table_With_WideIndex on EnforceForeignKeyIsIndexedRule_Table_With_WideIndex_Child(a,b)
+GO 
