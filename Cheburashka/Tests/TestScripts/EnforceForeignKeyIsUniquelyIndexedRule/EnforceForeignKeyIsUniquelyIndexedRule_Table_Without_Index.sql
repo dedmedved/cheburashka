5e4759313f1ed3d4f27dbd2e51335a2f4fc3c9ec
DM <DM@DM>
2021-03-29 11:46:16 +0100

    First draft - interesting problems found around clustered secondary indexes.

---
+++ Cheburashka/Tests/TestScripts/EnforceForeignKeyIsUniquelyIndexedRule/EnforceForeignKeyIsUniquelyIndexedRule_Table_Without_Index.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE dbo.EnforceForeignKeyIsIndexedRule_Table_Without_Index (a INT
+, CONSTRAINT PK_EnforceForeignKeyRule_Table_With_FK primary KEY (a)
+) ;
+GO
+create table EnforceForeignKeyIsIndexedRule_Table_Without_Index_Child (a int
+, constraint FK_EnforceForeignKeyIsIndexedRule_Table_Without_Index foreign key (a) references EnforceForeignKeyIsIndexedRule_Table_Without_Index (a)
+)
+go
