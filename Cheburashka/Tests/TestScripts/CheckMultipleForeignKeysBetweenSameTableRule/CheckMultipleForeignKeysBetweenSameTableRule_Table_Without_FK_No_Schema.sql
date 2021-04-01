8f55aba478a6336329469ac5cf6f35a7fbc40f0b
DM <DM@DM>
2021-04-01 15:32:44 +0100

    Added tests for rule DM0035

--- Cheburashka/Tests/TestScripts/CheckMultipleForeignKeysBetweenSameTableRule/CheckMultipleForeignKeysBetweenSameTableRule_Table_Without_FK_No_Schema.sql
+++
@@ -1,7 +0,0 @@
-﻿create table EnforceForeignKeyRule_Table_Without_FK_No_Schema (a int
-, CONSTRAINT PK_EnforceForeignKeyRule_Table_Without_FK_No_Schema primary KEY (a)
-)
-go
-create table EnforceForeignKeyRule_Table_Without_FK_No_Schema_Child (a int
-)
-go
