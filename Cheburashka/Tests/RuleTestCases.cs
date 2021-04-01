0b760764365f3c0c547a246f73b2183e5980037c
DM <DM@DM>
2021-04-01 15:32:44 +0100

    Added tests for rule DM0035

--- Cheburashka/Tests/RuleTestCases.cs
+++ Cheburashka/Tests/RuleTestCases.cs
@@ -1844,5 +1844,57 @@ namespace Cheburashka.Tests
             // Since this test verifies results against a baseline file, we don't need to do any extra verification
             test.RunTest(EnforceForeignKeyIsUniquelyIndexedRule.RuleId);
         }
+
+        /// <summary>
+        /// <para>
+        /// This test uses input scripts saved in the "TestScripts\EnforceForeignKeyIsUniquelyIndexedRule" folder and compares the
+        /// results to the "EnforceForeignKeyIsUniquelyIndexedRule-Baseline.txt file in that directory. If you wanted to add extra test cases
+        /// just add in new sql files and run the test. The failure message will include links to the output file - if all
+        /// the problems look correct there, then you can simply copy its contents into the baseline file and rerun the test.
+        /// </para>
+        /// <para>
+        /// This is a standard approach used inside the team and is very useful for testing rules since all you need is a tiny
+        /// amount of test code and some good examples that show where your rule should/should not highlight a problem.
+        /// </para>
+        /// </summary>
+        [TestMethod]
+        public void CheckMultipleForeignKeysBetweenSameTable_BIN()
+        {
+            using BaselinedRuleTest test = new BaselinedRuleTest(
+                TestContext,
+                nameof(CheckMultipleForeignKeysBetweenSameTableRule),
+                new TSqlModelOptions { Collation = "Latin1_General_BIN" },
+                SqlServerVersion.Sql100
+                );
+            // Since this test verifies results against a baseline file, we don't need to do any extra verification
+            test.RunTest(CheckMultipleForeignKeysBetweenSameTableRule.RuleId);
+        }
+
+        /// <summary>
+        /// <para>
+        /// This test uses input scripts saved in the "TestScripts\EnforceForeignKeyIsUniquelyIndexedRule" folder and compares the
+        /// results to the "EnforceForeignKeyIsUniquelyIndexedRule-Baseline.txt file in that directory. If you wanted to add extra test cases
+        /// just add in new sql files and run the test. The failure message will include links to the output file - if all
+        /// the problems look correct there, then you can simply copy its contents into the baseline file and rerun the test.
+        /// </para>
+        /// <para>
+        /// This is a standard approach used inside the team and is very useful for testing rules since all you need is a tiny
+        /// amount of test code and some good examples that show where your rule should/should not highlight a problem.
+        /// </para>
+        /// </summary>
+        [TestMethod]
+        public void CheckMultipleForeignKeysBetweenSameTable_CI_AI()
+        {
+            using BaselinedRuleTest test = new BaselinedRuleTest(
+                TestContext,
+                nameof(CheckMultipleForeignKeysBetweenSameTableRule),
+                new TSqlModelOptions { Collation = "Latin1_General_CI_AI" },
+                SqlServerVersion.Sql100
+                );
+            // Since this test verifies results against a baseline file, we don't need to do any extra verification
+            test.RunTest(CheckMultipleForeignKeysBetweenSameTableRule.RuleId);
+        }
+
+
     }
 }
