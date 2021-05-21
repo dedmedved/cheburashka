2c927912b607a1567294474bd4cd8a4e3be1335b
DM <DM@DM>
2021-05-21 14:59:36 +0100

    Added feature database

---
+++ FeaturesDatabase/sparse_columns/Tables/DocumentStore.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE sparse_columns.DocumentStore  
+    (DocID int PRIMARY KEY,  
+     Title varchar(200) NOT NULL,  
+     ProductionSpecification varchar(20) SPARSE NULL,  
+     ProductionLocation smallint SPARSE NULL,  
+     MarketingSurveyGroup varchar(20) SPARSE NULL ) ;  
+GO  
