76b61cefe0a009050bc4de76fb859afb1462a9a8
DM <DM@DM>
2021-06-09 18:22:46 +0100

    Finished insert from ( DML operation ) x changes and tests.

---
+++ Cheburashka/Tests/TestScripts/EnforceTableAliasRule/InsertFromDMLDeleteProc.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROC InsertFromDMLDeleteProc
+AS
+BEGIN
+    INSERT INTO TableC
+    SELECT	B1
+    ,		B2
+    FROM
+    (
+        DELETE TableB
+        OUTPUT	inserted.B1 AS B1
+        ,		inserted.B2 AS B2
+    )   z;
+
+
+    INSERT INTO TableC
+    SELECT	B1
+    ,		B2
+    FROM
+    (
+        DELETE TableB
+        OUTPUT	inserted.B1 AS B1
+        ,		inserted.B2 AS B2
+        FROM    TableB
+        ,       TableC
+    )   z;
+
+    DELETE TableB
+    OUTPUT	inserted.B1 AS B1
+    ,		inserted.B2 AS B2
+    FROM    TableB
+    ,       TableC
+
+END;
+
